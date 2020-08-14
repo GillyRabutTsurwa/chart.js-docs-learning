d4bd577a4911a7b43b4b43d1713375e14633a7e9
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-14 03:57:54 -0500

    radars. I love this fashion of representing data and I love Chart.js

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ let myChart = new Chart(chart, {
 	type: "radar",
 	data: {
 		labels: [ "Coding", "Designing", "Napping", "Running", "Driving", "Naturing" ],
+		//NEW: I JUST realised for the first time que l'on peut utiliser plusieurs jeux de données (datasets)
+		//NEW: To name your legend, you need to give each dataset a LABEL.
+		//NEW:NOTE: I understand datasets now. Dataset is one representation of data. We can have multiple of this in a graph. Just like we can have more than one bar for one section in bar graph. Chart.js is Cooooo
 		datasets: [
 			{
+				label: "Gilbert",
 				data: [ 20, 17, 27, 5, 19, 22 ],
-				backgroundColor: [ "rgba(255, 99, 132, 0.4)" ]
+				backgroundColor: [ "rgba(255, 99, 132, 0.5)" ]
+			},
+			{
+				label: "Denisa",
+				data: [ 10, 4, 15, 19, 6, 12 ],
+				backgroundColor: [ "rgba(54, 162, 235, 0.5)" ]
 			}
 		]
 	},
