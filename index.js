76c9e6b2d93a8a538914a156eb1f905f0ae254ac
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-14 02:56:55 -0500

    line-graph

--- index.js
+++ index.js
@@ -10,7 +10,38 @@ let myChart = new Chart(chart, {
 				lineTension: 0, // delete this and the lines will go back to smooth
 				backgroundColor: "transparent",
 				borderColor: "rgb(255, 99, 132)",
-				data: [ 0, 10, 5, 2, 20, 30, 15 ]
+				// data: [ 0, 10, 5, 2, 20, 30, 15 ]
+				//NOTE: The x value is guesstimated so there's not much point in setting them. The array is good enough. Plus, the x values don't show, either when you hover, nor in the x axis. But I like it because it looks more explicit.
+				data: [
+					{
+						x: 0,
+						y: 10
+					},
+					{
+						x: 5,
+						y: 15
+					},
+					{
+						x: 10,
+						y: 25
+					},
+					{
+						x: 15,
+						y: 10
+					},
+					{
+						x: 20,
+						y: 35
+					},
+					{
+						x: 25,
+						y: 20
+					},
+					{
+						x: 30,
+						y: 20
+					}
+				]
 			}
 		]
 	}
