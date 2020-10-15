a706e69d601376de8ab91063d55c68e94a90f782
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-10-14 20:49:47 -0500

    review

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
+// NOTE: Targeting our canvas element, where our graph sera,
 const chart = document.getElementById("myFirstChart").getContext("2d");
 
+// NOTE: Making a chart with the Chart Object.
 let myChart = new Chart(chart, {
-	// The type of chart we want to create
-	type: "line",
+  // NOTE: The type of chart we want to create
+  type: "line",
 
-	// The data for our dataset
-	data: {
-		labels: [ "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet" ],
-		datasets: [
-			{
-				label: "My first dataset",
-				backgroundColor: "rgb(255, 99, 132)",
-				borderColor: "rgb(255, 99, 132)",
-				data: [ 0, 10, 5, 2, 20, 30, 45 ]
-			}
-		]
-	},
+  // NOTE: The data object, which will contain all the useful info for our graph, including labels and datasets.
+  data: {
+    labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet"],
+    // NOTE: dataset is the set of data for a particular [whatever] in the graph.
+    // NOTE: can have one or multiple
+    datasets: [
+      {
+        label: "My first dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(0, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  },
 
-	// Configuration options go here.
-	// So far, Sielewi chochote yenye ikuwayo hapa.
-	options: {
-		scales: {
-			yAxes: [
-				{
-					ticks: {
-						beginAtZero: true
-					}
-				}
-			]
-		}
-	}
+  // NOTE: Special configuration for our graphs go here
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
 });
